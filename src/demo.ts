6ff7bd5e5dff5d2d432b317f78caf5eccf527b14
alexbadm <eto.ya@list.ru>
2018-01-03 02:15:22 +0300

    Your key and your secret

--- src/demo.ts
+++ src/demo.ts
@@ -1,9 +1,6 @@
 import ExchangeState from './index';
 
 const state = new ExchangeState();
-state.auth(
-  'w7Jd6Vp6cnyzGcc45BgCyje5EDeWFjieOezuKARlvOY',
-  'fqoe1P1yrP23cLtsLGKnoLqJ93XGRAE7ZbQWbZFqf0B');
-
+state.auth('your key', 'your secret');
 state.subscribeTicker('IOTUSD');
 state.start();
